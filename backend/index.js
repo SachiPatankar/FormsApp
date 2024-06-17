ffa6f543d8a3baee8f5ec213050fc29fa3788ce8
Sachi Patankar <sachipatankar19@gmail.com>
2024-06-17 16:15:06 +0530

    All done

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: ["http://localhost:5173"]
+    origin: ["http://localhost:5173","http://localhost:5174"]
   }));
   
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
